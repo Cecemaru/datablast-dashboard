bce2d8a6cc68d0931695ca6abea9c28dac3c66cc
cecem <cece@cece.com>
2023-12-17 13:32:19 +0300

    axios eklendi, env dosyasinda base url olusturuldu ve home sayfasinda header uc noktasina istek atilip bilgiler gosterildi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,65 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const base = import.meta.env.VITE_BASE_URL
+
+const headers = ref({})
+
+const selectDetails = ref({
+  headerName: null as string | null,
+  headerValue: null as string | null,
+  headerId: null as number | null
+})
+
+const openDetail = (value: string, key: string, index: number) => {
+  selectDetails.value = {
+    headerName: key,
+    headerValue: value,
+    headerId: index
+  }
+}
+
+onMounted(async () => {
+  await fetchHeader()
+})
+
+const fetchHeader = async () => {
+  try {
+    const response = await axios.get(`${base}/headers`)
+    headers.value = response.data.headers
+  } catch (error) {
+    console.error('Wrong fetch error', error)
+  }
+}
+</script>
 
 <template>
-  <main>Home</main>
+  <div>
+    <h2>Headers Uc Noktasindan Gelen Istek</h2>
+
+    <table class="table table-hover table-striped">
+      <thead>
+        <tr>
+          <th class="col-1">#</th>
+          <th class="col-4">Header Adı</th>
+          <th class="col-7">Header Değeri</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(value, key, index) in headers"
+          :key="index"
+          data-bs-toggle="collapse"
+          :data-bs-target="'#collapseExample' + index"
+          aria-expanded="false"
+          @click="openDetail(value, key, index)"
+        >
+          <td>{{ index + 1 }}</td>
+          <td>{{ key }}</td>
+          <td>{{ value }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
