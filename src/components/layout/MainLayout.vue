fabc028cabc9819ebc4e06af1a26c97d4b45fadc
cecem <cece@cece.com>
2023-12-17 13:13:12 +0300

    vertical navbar hazirlandi ve datablast logosu eklendi

--- src/components/layout/MainLayout.vue
+++ src/components/layout/MainLayout.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import VerticalNavbar from '@/components/layout/ui/VerticalNavbar.vue'
+</script>
 
 <template>
   <div class="d-flex bg-secondary-subtle">
-    <div class="d-none d-lg-block w-25">cecemaru</div>
+    <div class="d-none d-lg-block w-25">
+      <VerticalNavbar />
+    </div>
 
     <div class="w-100 mx-3">
       <router-view></router-view>
---
+++ src/components/layout/ui/VerticalNavbar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Logo from '@/assets/datablast.png'
+</script>
+
+<template>
+  <div class="border vh-100 sticky-top bg-white">
+    <div class="p-4 my-2">
+      <router-link class="" to="/"><img v-bind:src="Logo" alt="Logo" /></router-link>
+    </div>
+
+    <nav class="d-flex justify-content-center">
+      <div class="list-group col-11">
+        <router-link class="list-group-item" to="/">Home</router-link>
+
+        <router-link class="list-group-item" to="/login">Login</router-link>
+
+        <router-link class="list-group-item" to="/profile">Profile</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
